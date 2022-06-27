b74d63b7f7b7e6ba1b66e1e97b9557a54f46e95e
Maurice <maurice.baez1@gmail.com>
2022-06-27 15:01:02 -0700

    fixed newListingForm, added textarea for desc

--- src/NewListingForm.js
+++ src/NewListingForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import "./NewListingForm.css";
 
 function NewListingForm({ addListing }) {
   const initialFormData = {
@@ -7,7 +8,7 @@ function NewListingForm({ addListing }) {
     description: "",
     location: "",
     type: "house",
-    price_per_night: ""
+    price_per_night: "",
   };
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState(initialFormData);
@@ -21,22 +22,20 @@ function NewListingForm({ addListing }) {
   }
 
   function handleChange(evt) {
-    if (evt.target.files){
+    if (evt.target.files) {
       const uploadFiles = evt.target.files;
       setFiles([...uploadFiles]);
-
-    } else{
+    } else {
       const { name, value } = evt.target;
-      setFormData(data => ({ ...data, [name]: value }));
+      setFormData((data) => ({ ...data, [name]: value }));
     }
-
   }
 
   return (
     <div className="NewListingForm">
-      <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+      <div className="container card__container mt-3">
         <h2 className="mb-3">Add a New Listing</h2>
-        <div className="card">
+        <div className="card newListing__card">
           <div className="card-body">
             <form onSubmit={handleSubmit}>
               <div className="mb-3">
@@ -50,8 +49,8 @@ function NewListingForm({ addListing }) {
               </div>
               <div className="mb-3">
                 <label className="form-label">Description</label>
-                <input
-                  type="textarea"
+                <textarea
+                  rows="5"
                   name="description"
                   className="form-control"
                   value={formData.description}
@@ -74,8 +73,8 @@ function NewListingForm({ addListing }) {
                   name="type"
                   className="form-control"
                   value={formData.type}
-                  onChange={handleChange}>
-
+                  onChange={handleChange}
+                >
                   <option value="house">House</option>
                   <option value="apartment">Apartment</option>
                   <option value="tent">Tent</option>
@@ -85,7 +84,6 @@ function NewListingForm({ addListing }) {
                   <option value="backyard">Backyard</option>
                   <option value="castle">Castle</option>
                   <option value="igloo">Igloo</option>
-
                 </select>
               </div>
               <div className="mb-3">
@@ -95,7 +93,7 @@ function NewListingForm({ addListing }) {
                   className="form-control"
                   value={formData.price_per_night}
                   onChange={handleChange}
-                /> 
+                />
               </div>
               <div className="mb-3">
                 <label htmlFor="images">Images</label>
@@ -108,7 +106,10 @@ function NewListingForm({ addListing }) {
                 />
               </div>
               <div className="d-grid">
-                <button className="btn btn-secondary" onClick={handleSubmit}>
+                <button
+                  className="btn btn-outline-secondary newLisitng__btn"
+                  onClick={handleSubmit}
+                >
                   Submit
                 </button>
               </div>
@@ -121,5 +122,3 @@ function NewListingForm({ addListing }) {
 }
 
 export default NewListingForm;
-
-
